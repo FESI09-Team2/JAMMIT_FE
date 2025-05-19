79a98420ccd09526fb58f18e2566973ed58d9879
whdjh <whdjh98@naver.com>
2025-05-19 16:49:35 +0900

    feat: 텍스트 타입, placeholder, RHF 적용

--- src/app/testpage/input/page.tsx
+++ src/app/testpage/input/page.tsx
@@ -1,12 +1,31 @@
-import Input from '@/components/commons/input';
+'use client';
+
+import { useForm } from 'react-hook-form';
+import Input from '@/components/commons/Input';
+
+type FormValues = {
+  email: string;
+};
 
 export default function Home() {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  // TODO: 제출로 변경 예정(data가 잘 나오는지 확인을 위해 사용)
+  const onSubmit = (data: FormValues) => {
+    alert(data.email);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       {/* text */}
-      <Input />
+      <Input
+        type="text"
+        placeholder="텍스트를 입력하세요."
+        register={register('email')}
+      />
       {/* password */}
       {/*<Input />;*/}
-    </>
+      <button type="submit">제출</button>
+    </form>
   );
 }
--- src/components/commons/input.tsx
+++ src/components/commons/input.tsx
@@ -1,5 +1,35 @@
 'use client';
 
-export default function Input() {
-  return <div>input 컴포넌트</div>;
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+interface InputProps {
+  type: 'text' | 'password';
+  placeholder: string;
+  register: UseFormRegisterReturn;
+}
+
+export default function Input({
+  type,
+  placeholder = '',
+  register,
+}: InputProps) {
+  const renderInput = () => {
+    switch (type) {
+      case 'text':
+        return (
+          <input
+            type="text"
+            placeholder={placeholder}
+            {...register}
+            className="w-full rounded-md border px-3 py-2"
+          />
+        );
+      case 'password':
+        return <div>패스워드</div>;
+      default:
+        return null;
+    }
+  };
+
+  return <div>{renderInput()}</div>;
 }
