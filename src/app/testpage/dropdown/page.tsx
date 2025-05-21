e7f37d5c8b0b8ff2460a414a0161faeda47db2fb
whdjh <whdjh98@naver.com>
2025-05-21 10:01:43 +0900

    feat: 드롭다운 추가

--- src/app/testpage/dropdown/page.tsx
+++ src/app/testpage/dropdown/page.tsx
@@ -1,8 +1,33 @@
 'use client';
 
-import React from 'react';
-//import Dropdown from '@/components/commons/Dropdown';
+import { useState } from 'react';
+import Dropdown from '@/components/commons/Dropdown';
+import useSortedData from '@/hooks/useSortByOption';
+import ArrowDown from '@/assets/icons/ic_arrowdown.svg';
+
+const OPTION = ['최신 순', '리뷰 높은 순', '참여 인원 순'];
 
 export default function Home() {
-  return <div>드롭다운테스트페이지</div>;
+  const [sortOption, setSortOption] = useState(OPTION[0]);
+  const sortedData = useSortedData(sortOption);
+
+  return (
+    <>
+      <Dropdown
+        onSelect={setSortOption}
+        menuOptions={OPTION} // 여기서 정렬 옵션을 설정
+        prefixIcon={<ArrowDown />} // 지정하지 않으면 기본 아이콘 사용
+      />
+      <div className="mt-4 space-y-2">
+        {sortedData.map((item) => (
+          <div key={item.id} className="rounded-lg border px-4 py-2 shadow-sm">
+            <p className="font-semibold">{item.title}</p>
+            <p>리뷰: {item.review}</p>
+            <p>참여 인원: {item.participants}</p>
+            <p>작성일: {item.createdAt}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/components/commons/Dropdown.tsx
+++ src/components/commons/Dropdown.tsx
@@ -1,1 +1,71 @@
 'use client';
+
+import { ReactNode, useRef, useState } from 'react';
+import SurfixIcon from '@/assets/icons/ic_sort.svg';
+import { useClickOutside } from '@/hooks/useClickOutside';
+
+interface DropdownProps {
+  onSelect: (selectedDropdownMenu: string) => void;
+  menuOptions: string[];
+  prefixIcon?: ReactNode;
+}
+
+export default function Dropdown({
+  onSelect,
+  menuOptions,
+  prefixIcon,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedDropdownMenu, setSelectedDropdownMenu] = useState('최신 순');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useClickOutside(dropdownRef, () => setIsOpen(false));
+
+  const handleDropdownMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (DropdownMenu: string) => {
+    setSelectedDropdownMenu(DropdownMenu);
+    setIsOpen(false);
+    onSelect(DropdownMenu);
+  };
+
+  return (
+    // TODO: 글자나 시안 확정됨에 따라 w-auto랑 h-auto 수정
+    <div className="h-auto w-auto" ref={dropdownRef}>
+      <div className="relative">
+        <button
+          onClick={handleDropdownMenu}
+          className="flex items-center justify-between gap-[0.625rem] rounded-xl border border-gray-100 px-[0.75rem] py-[0.5rem] text-gray-800"
+        >
+          {prefixIcon ? (
+            <>
+              {prefixIcon}
+              <span>{selectedDropdownMenu}</span>
+            </>
+          ) : (
+            <>
+              <span className="hidden sm:inline">{selectedDropdownMenu}</span>
+              <SurfixIcon />
+            </>
+          )}
+        </button>
+
+        {isOpen && (
+          <div className="absolute gap-[0.625rem] rounded-xl border border-gray-100 text-gray-800">
+            {menuOptions.map((option) => (
+              <div
+                key={option}
+                onClick={() => handleSelect(option)}
+                className="cursor-pointer rounded-xl px-[0.75rem] py-[0.5rem] hover:bg-orange-100"
+              >
+                <span>{option}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
