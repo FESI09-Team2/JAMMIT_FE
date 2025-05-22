863fde81c8c7d3a848210e15d4b29e379925381d
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-05-22 15:21:06 +0900

    feat: 로그인 페이지 스타일 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,56 +28,65 @@ export default function LoginPage() {
 
   return (
     <div className="mt-[12rem] flex items-center justify-center">
-      <div className="flex w-[25.125rem] flex-col items-center">
-        <FormProvider {...methods}>
-          <h1 className="text-[1.5rem] font-semibold">로그인</h1>
-          <form
-            onSubmit={methods.handleSubmit(onSubmit)}
-            noValidate
-            className="w-full"
-          >
-            <Input
-              name="email"
-              type="text"
-              label="아이디"
-              placeholder="이메일을 입력해주세요."
-              rules={{
-                required: '이메일은 필수 입력입니다.',
-                pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                  message: '올바른 이메일 형식을 입력해주세요.',
-                },
-              }}
-            />
-            <Input
-              name="password"
-              type="password"
-              label="비밀번호"
-              placeholder="비밀번호를 입력해주세요."
-              rules={{
-                required: '비밀번호는 필수 입력입니다.',
-                minLength: {
-                  value: 8,
-                  message: '비밀번호는 최소 8자 이상이어야 합니다.',
-                },
-              }}
-            />
-            <Button
-              variant="solid"
-              size="large"
-              className="mt-5 w-full"
-              type="submit"
-              disabled={!isValid}
-            >
+      <div className="flex h-[30.125rem] w-[31.875rem] items-center justify-center rounded-[1.5rem] bg-[#242429]">
+        <div className="flex w-[25.125rem] flex-col items-center">
+          <FormProvider {...methods}>
+            <h1 className="mb-[2rem] text-[1.5rem] font-semibold text-gray-100">
               로그인
-            </Button>
-          </form>
-        </FormProvider>
-        <div className="mt-5 flex justify-center">
-          <p className="mr-1">JAMMIT이 처음이신가요?</p>
-          <Link href="/signup/step1" className="underline underline-offset-2">
-            회원가입
-          </Link>
+            </h1>
+            <form
+              onSubmit={methods.handleSubmit(onSubmit)}
+              noValidate
+              className="w-full"
+            >
+              <Input
+                name="email"
+                type="text"
+                label="아이디"
+                placeholder="이메일을 입력해주세요."
+                rules={{
+                  required: '이메일은 필수 입력입니다.',
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: '올바른 이메일 형식을 입력해주세요.',
+                  },
+                }}
+              />
+              <Input
+                name="password"
+                type="password"
+                label="비밀번호"
+                placeholder="비밀번호를 입력해주세요."
+                rules={{
+                  required: '비밀번호는 필수 입력입니다.',
+                  minLength: {
+                    value: 8,
+                    message: '비밀번호는 최소 8자 이상이어야 합니다.',
+                  },
+                }}
+              />
+              <Button
+                variant="solid"
+                size="large"
+                className="mt-[2.5rem] w-full"
+                type="submit"
+                disabled={!isValid}
+              >
+                로그인
+              </Button>
+            </form>
+          </FormProvider>
+          <div className="mt-[2.5rem] flex justify-center">
+            <p className="mr-1 text-[0.938rem] font-medium text-gray-400">
+              JAMMIT이 처음이신가요?
+            </p>
+            <Link
+              href="/signup/step1"
+              className="text-[0.938rem] font-medium text-[#BF52FF] underline underline-offset-2"
+            >
+              회원가입
+            </Link>
+          </div>
         </div>
       </div>
     </div>
