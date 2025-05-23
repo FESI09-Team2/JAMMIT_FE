6ed0ef640d21905a3fdfdff562edb28735735ad1
강은우 <80903685+euoonw@users.noreply.github.com>
2025-05-23 13:10:32 +0900

    feat: [페이지] 로그인, 회원가입 페이지 디자인 적용 (#31)
    * feat: 로그인 페이지 스타일 추가
    
    * refactor: 로그인페이지 파일 컴포넌트로 분리해서 정리
    
    * refactor:  auth 공통 프레임 공통 카드컴포넌트로 분리
    
    * feat: 회원가입step1 페이지 ui
    
    * test: 회원가입 step1 페이지 테스트 추가
    
    ---------
    
    Co-authored-by: 강은우 <80903685+eunwoo66@users.noreply.github.com>

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -2,9 +2,9 @@ import Gnb from './Gnb';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="min-h-screen bg-[#1A1A1E]">
       <Gnb />
       <main className="p-[3.75rem]">{children}</main>
-    </>
+    </div>
   );
 }
