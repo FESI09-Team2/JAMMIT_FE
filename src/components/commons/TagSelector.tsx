81982b309afacc5f9ba98920b5fcd3f3bc31fef0
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-05-23 15:05:19 +0900

    feat: useMemo, useCallback 코드리뷰 반영

--- src/components/commons/TagSelector.tsx
+++ src/components/commons/TagSelector.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import CheckIcon from '@/assets/icons/ic_check.svg';
 import PlusIcon from '@/assets/icons/ic_plus.svg';
 import { clsx } from 'clsx';
@@ -11,36 +11,39 @@ interface TagSelectorProps {
 
 export default function TagSelector({ tags, onChange }: TagSelectorProps) {
   const [selected, setSelected] = useState<string[]>([]);
+  const selectedSet = useMemo(() => new Set(selected), [selected]);
 
-  const toggleTag = (tag: string) => {
-    setSelected((prev) => {
-      const exists = prev.includes(tag);
-      const updated = exists ? prev.filter((t) => t !== tag) : [...prev, tag];
-      onChange?.(updated);
-      return updated;
-    });
-  };
-
-  const isSelected = (tag: string) => selected.includes(tag);
+  const toggleTag = useCallback(
+    (tag: string) => {
+      setSelected((prev) => {
+        const exists = prev.includes(tag);
+        const updated = exists ? prev.filter((t) => t !== tag) : [...prev, tag];
+        onChange?.(updated);
+        return updated;
+      });
+    },
+    [onChange],
+  );
 
   return (
     <div>
       <div className="flex flex-wrap gap-[0.5rem]">
-        {tags.map((tag) => (
-          <button
-            key={tag}
-            onClick={() => toggleTag(tag)}
-            className={clsx(
-              'box-border flex h-[2rem] items-center justify-center rounded-[0.5rem] border bg-[#34343a] pr-[0.5rem] pl-[0.75rem] text-sm font-medium text-gray-100',
-              isSelected(tag)
-                ? 'border-[#9747FF] shadow-md'
-                : 'border-transparent',
-            )}
-          >
-            {tag}
-            {isSelected(tag) ? <CheckIcon /> : <PlusIcon />}
-          </button>
-        ))}
+        {tags.map((tag) => {
+          const isActive = selectedSet.has(tag);
+          return (
+            <button
+              key={tag}
+              onClick={() => toggleTag(tag)}
+              className={clsx(
+                'box-border flex h-[2rem] items-center justify-center rounded-[0.5rem] border bg-[#34343a] pr-[0.5rem] pl-[0.75rem] text-sm font-medium text-gray-100',
+                isActive ? 'border-[#9747FF] shadow-md' : 'border-transparent',
+              )}
+            >
+              {tag}
+              {isActive ? <CheckIcon /> : <PlusIcon />}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
