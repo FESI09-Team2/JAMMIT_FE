1609c73c1e49208bd4f4c5ebcfe575fead61c3c8
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-05-26 15:11:34 +0900

    refactor: 가독성, 재사용성, 안정성 고려 코드 리팩토링

--- src/components/commons/TagSelector.tsx
+++ src/components/commons/TagSelector.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import CheckIcon from '@/assets/icons/ic_check.svg';
 import PlusIcon from '@/assets/icons/ic_plus.svg';
 import { clsx } from 'clsx';
@@ -13,7 +13,7 @@ interface TagSelectorProps {
 
 export default function TagSelector({
   tags,
-  mode,
+  mode = 'selectable',
   readonlySelected,
   onChange,
 }: TagSelectorProps) {
@@ -25,33 +25,36 @@ export default function TagSelector({
 
   const toggleTag = useCallback(
     (tag: string) => {
-      if (mode === 'readonly') {
-        return;
-      }
-      setSelected((prev) => {
-        const exists = prev.includes(tag);
-        const updated = exists ? prev.filter((t) => t !== tag) : [...prev, tag];
-        onChange?.(updated);
-        return updated;
-      });
+      if (mode === 'readonly') return;
+      setSelected((prev) =>
+        prev.includes(tag) ? prev.filter((t) => t !== tag) : [...prev, tag],
+      );
     },
-    [onChange],
+    [mode],
   );
+
+  useEffect(() => {
+    if (mode === 'selectable') {
+      onChange?.(selected);
+    }
+  }, [selected, onChange, mode]);
 
   return (
     <div>
       <div className="flex flex-wrap gap-[0.5rem]">
         {tags.map((tag, index) => {
           const isActive = selectedSet.has(tag);
+          const baseClass =
+            'box-border flex h-[2rem] items-center justify-center rounded-[0.5rem] border bg-[#34343a] pr-[0.5rem] pl-[0.75rem] text-sm font-medium text-gray-100';
+          const activeClass = isActive
+            ? 'border-[#9747FF] shadow-md'
+            : 'border-transparent';
+          const pointerClass = mode === 'selectable' ? 'cursor-pointer' : '';
           return (
             <button
               key={`${tag}-${index}`}
               onClick={() => toggleTag(tag)}
-              className={clsx(
-                'box-border flex h-[2rem] items-center justify-center rounded-[0.5rem] border bg-[#34343a] pr-[0.5rem] pl-[0.75rem] text-sm font-medium text-gray-100',
-                isActive ? 'border-[#9747FF] shadow-md' : 'border-transparent',
-                mode === 'selectable' && 'cursor-pointer',
-              )}
+              className={clsx(baseClass, activeClass, pointerClass)}
             >
               {tag}
               {isActive ? <CheckIcon /> : <PlusIcon />}
