28b260918c7a114499c3491abcdfe2ce5d5b8955
whdjh <whdjh98@naver.com>
2025-05-27 17:18:57 +0900

    feat: 장르 태그 추가

--- src/app/jam/page.tsx
+++ src/app/jam/page.tsx
@@ -15,7 +15,7 @@ export default function JamPage() {
       place: '',
       day: '',
       end: '',
-      tag: [],
+      genre: [],
       introduction: '',
     },
     mode: 'onChange',
--- src/components/products/jam/JamFormSection.tsx
+++ src/components/products/jam/JamFormSection.tsx
@@ -2,8 +2,10 @@
 
 import Input from '@/components/commons/Input';
 import TextArea from '@/components/commons/Textarea';
+import Button from '@/components/commons/Button';
+import TagSelector from '@/components/commons/TagSelector';
 import { Controller } from 'react-hook-form';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import {
   Control,
   UseFormRegister,
@@ -11,6 +13,7 @@ import {
   UseFormSetValue,
 } from 'react-hook-form';
 import { JamFormData } from '@/types/modal';
+import { GENRE_TAGS } from '@/constants/tags';
 
 interface JamFormSectionProps {
   control: Control<JamFormData>;
@@ -35,9 +38,17 @@ export default function JamFormSection({
     }
   }, [endDate, dayDate, setValue]);
 
+  const handleTagChange = useCallback(
+    (selectedTags: string[]) => {
+      setValue('genre', selectedTags);
+    },
+    [setValue],
+  );
+
   return (
     <div className="flex h-[57.5rem] w-[61rem] flex-col bg-[rgba(32,32,36,1)] p-[2.5rem]">
       <div className="flex flex-col gap-[1.5rem]">
+        {/** 모임 제목 */}
         <Input
           name="jamName"
           type="text"
@@ -46,6 +57,8 @@ export default function JamFormSection({
           rules={{ required: '모임 제목을 입력하세요.' }}
         />
 
+        {/** TODO: kakaomap 검색 */}
+        {/** 장소 */}
         <Input
           name="place"
           type="text"
@@ -61,7 +74,19 @@ export default function JamFormSection({
         />
 
         {/** TODO: input과 캘린더 커스텀해야됨 */}
+        {/** 날짜 */}
         <div className="flex gap-[1.25rem]">
+          <div className="flex flex-col gap-[0.5rem]">
+            <label htmlFor="end" className="font-semibold">
+              모집 마감일
+            </label>
+            <input
+              id="end"
+              type="date"
+              {...register('end')}
+              className="h-[2.75rem] w-[13.125rem] rounded border px-[1rem] py-[0.625rem]"
+            />
+          </div>
           <div className="flex flex-col gap-[0.5rem]">
             <label htmlFor="day" className="font-semibold">
               모임 날짜
@@ -82,44 +107,53 @@ export default function JamFormSection({
               className="h-[2.75rem] w-[13.125rem] rounded border px-[1rem] py-[0.625rem]"
             />
           </div>
-          <div className="flex flex-1 flex-col gap-[0.5rem]">
-            <label htmlFor="end" className="font-semibold">
-              모집 마감일
-            </label>
-            <input
-              id="end"
-              type="date"
-              {...register('end')}
-              className="h-[2.75rem] w-[13.125rem] rounded border px-[1rem] py-[0.625rem]"
-            />
+        </div>
+      </div>
+      <hr className="mx-auto my-[2.5rem] w-[56rem] bg-gray-800" />
+
+      {/** 모집 세션 */}
+      <div className="flex flex-col gap-[0.5rem]">
+        <p className="text-lg font-semibold">모집 세션</p>
+        <div className="flex justify-between">
+          <div className="flex gap-[0.75rem]">
+            {/** 드롭다운 자리 */}
+            <input placeholder="드롭다운 자리임" type="number" />
+            {/** 인원 input자리 */}
+            <input placeholder="인원자리임" type="number" />
+          </div>
+          {/** 버튼 자리 */}
+          <div className="flex gap-[0.75rem]">
+            <Button variant="outline" size="small">
+              임시 저장
+            </Button>
+            <Button variant="outline" size="small">
+              삭제
+            </Button>
           </div>
         </div>
       </div>
       <hr className="mx-auto my-[2.5rem] w-[56rem] bg-gray-800" />
 
-      <div className="flex flex-col gap-[0.5rem]">
-        <p className="text-lg font-semibold">모집 세션</p>
-        <div>
-          {/** 드롭다운자리 */}
-          {/** 인원 input자리 */}
-          {/** 버튼 자리 */}
-        </div>
-      </div>
-      <hr className="mx-auto my-[2.5rem] w-[56rem] bg-gray-800" />
-
+      {/** 모임 장르 태그 */}
       <div className="flex flex-col gap-[0.5rem]">
         <p className="text-lg font-semibold">모임 장르</p>
+        <TagSelector
+          mode="selectable"
+          tags={GENRE_TAGS}
+          onChange={handleTagChange}
+        />
       </div>
       <hr className="mx-auto my-[2.5rem] w-[56rem] bg-gray-800" />
 
+      {/** 모임 소개 */}
       <div className="flex flex-col gap-[0.5rem]">
-        <p className="text-lg font-semibold">세션 소개</p>
+        <p className="text-lg font-semibold">소개글</p>
         <Controller
           name="introduction"
           control={control}
           render={({ field }) => (
             <TextArea
-              placeholder="세션에 대한 간단한 소개 남겨주세요."
+              placeholder="어떤 일이 일어날까요?"
               value={field.value}
               onChange={field.onChange}
             />
