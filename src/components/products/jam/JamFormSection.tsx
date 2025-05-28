4ce3563df5370cacb738ece08d800addf2d4be46
whdjh <whdjh98@naver.com>
2025-05-28 16:15:45 +0900

    feat: form높이 수정

--- src/components/products/jam/JamFormSection.tsx
+++ src/components/products/jam/JamFormSection.tsx
@@ -67,7 +67,7 @@ export default function JamFormSection({
   );
 
   return (
-    <div className="flex h-[57.5rem] w-[61rem] flex-col bg-[#202024] p-[2.5rem]">
+    <div className="flex h-[60.75rem] w-[61rem] flex-col bg-[#202024] p-[2.5rem]">
       <div className="flex flex-col gap-[1.5rem]">
         {/** 모임 제목 */}
         <Input
@@ -78,7 +78,6 @@ export default function JamFormSection({
           rules={{ required: '모임 제목을 입력하세요.' }}
         />
 
-        {/** TODO: kakaomap 검색 */}
         {/** 장소 */}
         <SearchInput value={place} onChange={(val) => setValue('place', val)} />
 
