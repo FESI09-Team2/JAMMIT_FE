7bc4e84f45fe977e748feba93e3ca66c0868fd25
whdjh <whdjh98@naver.com>
2025-06-02 17:40:12 +0900

    feat: 레이아웃 완성

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,11 +3,20 @@
 import clsx from 'clsx';
 import { useQueryTab } from '@/hooks/useQueryTab';
 import UserCard from '@/components/products/mypage/UserCard';
+import Participating from '@/components/products/mypage/Participating';
+import Created from '@/components/products/mypage/Created';
+import ReviewsReceived from '@/components/products/mypage/ReviewsReceived';
+import ReviewsToWrite from '@/components/products/mypage/ReviewsToWrite';
 
 export default function MyPage() {
   const { activeTab, setTab } = useQueryTab<
-    'participating' | 'received' | 'towrite'
-  >('tab', 'participating', ['participating', 'received', 'towrite']);
+    'participating' | 'created' | 'reviews_received' | 'reviews_towrite'
+  >('tab', 'participating', [
+    'participating',
+    'created',
+    'reviews_received',
+    'reviews_towrite',
+  ]);
 
   const tabClass = (isActive: boolean) =>
     clsx(
@@ -38,11 +47,26 @@ export default function MyPage() {
         </div>
         <div className="flex items-center gap-[0.2rem]">
           <button
-            onClick={() => setTab('received')}
+            onClick={() => setTab('created')}
             className={clsx(
               'flex items-center gap-[0.25rem]',
-              tabClass(activeTab === 'received'),
-              activeTab === 'received' ? 'text-purple-500' : 'text-gray-400',
+              tabClass(activeTab === 'created'),
+              activeTab === 'created' ? 'text-purple-500' : 'text-gray-400',
+            )}
+          >
+            {/** API 데이터 교체 필요 */}
+            {`내가 만든 모임 ${2}`}
+          </button>
+        </div>
+        <div className="flex items-center gap-[0.2rem]">
+          <button
+            onClick={() => setTab('reviews_received')}
+            className={clsx(
+              'flex items-center gap-[0.25rem]',
+              tabClass(activeTab === 'reviews_received'),
+              activeTab === 'reviews_received'
+                ? 'text-purple-500'
+                : 'text-gray-400',
             )}
           >
             {/** API 데이터 교체 필요 */}
@@ -51,11 +75,13 @@ export default function MyPage() {
         </div>
         <div className="flex items-center gap-[0.2rem]">
           <button
-            onClick={() => setTab('towrite')}
+            onClick={() => setTab('reviews_towrite')}
             className={clsx(
               'flex items-center gap-[0.25rem]',
-              tabClass(activeTab === 'towrite'),
-              activeTab === 'towrite' ? 'text-purple-500' : 'text-gray-400',
+              tabClass(activeTab === 'reviews_towrite'),
+              activeTab === 'reviews_towrite'
+                ? 'text-purple-500'
+                : 'text-gray-400',
             )}
           >
             {/** API 데이터 교체 필요 */}
@@ -63,6 +89,11 @@ export default function MyPage() {
           </button>
         </div>
       </div>
+
+      {activeTab === 'participating' && <Participating />}
+      {activeTab === 'created' && <Created />}
+      {activeTab === 'reviews_received' && <ReviewsReceived />}
+      {activeTab === 'reviews_towrite' && <ReviewsToWrite />}
     </>
   );
 }
