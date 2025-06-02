b0730b9327691f7c0aa01b7a906f7cb24b3a3a07
whdjh <whdjh98@naver.com>
2025-06-02 18:18:53 +0900

    chore: px -> rem

--- src/components/products/mypage/UserCard.tsx
+++ src/components/products/mypage/UserCard.tsx
@@ -23,37 +23,37 @@ export default function UserCard() {
 
   return (
     <>
-      <div className="flex h-[250px] w-[full] items-center justify-center gap-[53px] bg-[#36114E]">
+      <div className="flex h-[15.625rem] w-[full] items-center justify-center gap-[3.3125rem] bg-[#36114E]">
         <div>
           <DefaultProfileImage width={128} height={128} />
         </div>
         <div className="flex flex-col text-gray-100">
-          <div className="flex items-center gap-[10px]">
+          <div className="flex items-center gap-[0.625rem]">
             {/** API 수정된 후 불러오게 수정 */}
-            <p className="text-[24px] leading-[38.4px] font-bold">
+            <p className="text-[1.5rem] leading-[2.4rem] font-bold">
               사용자닉네임
             </p>
             <button type="submit" onClick={handleProfileEdit}>
               <EditIcon width={18} height={18} />
             </button>
           </div>
-          <div className="flex items-center gap-[16px] text-sm font-medium">
+          <div className="flex items-center gap-[1rem] text-sm font-medium">
             <p>담당 세션</p>
             {/** API 수정된 후 여러개 불러오게 수정 */}
-            <div className="h-[32px] w-auto rounded-lg bg-[#34343A] px-[12px] py-[6px]">
+            <div className="h-[2rem] w-auto rounded-lg bg-[#34343A] px-[0.75rem] py-[0.375rem]">
               일렉 기타
             </div>
-            <div className="h-[20px] w-[1.5px] bg-gray-500" />
+            <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>선호 장르</p>
             {/** API 수정된 후 여러개 불러오게 수정 */}
-            <div className="h-[32px] w-auto rounded-lg bg-[#34343A] px-[12px] py-[6px]">
+            <div className="h-[2rem] w-auto rounded-lg bg-[#34343A] px-[0.75rem] py-[0.375rem]">
               팝
             </div>
-            <div className="h-[20px] w-[1.5px] bg-gray-500" />
+            <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>개설모임수</p>
             {/** API 수정된 후 불러오게 수정 아직 없음 */}
             <p>8</p>
-            <div className="h-[20px] w-[1.5px] bg-gray-500" />
+            <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>작성글수</p>
             {/** API 수정된 후 불러오게 수정 아직 없음 */}
             <p>20</p>
