61da8dd1263449917f6d08ae9c89641f0250f1e5
whdjh <whdjh98@naver.com>
2025-06-03 13:39:18 +0900

    feat: 내가 만든 모임도 무한 스크롤 적용

--- src/components/products/mypage/Created.tsx
+++ src/components/products/mypage/Created.tsx
@@ -1,31 +1,59 @@
+'use clients';
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Card } from '@/components/commons/Card';
 import { CARD_STATE } from '@/constants/card';
 import { RecruitCardData } from '@/types/card';
 import { mockCard } from './mockCard';
+import InfinityScroll from './InfinityScroll';
+
+const LOAD_SIZE = 8;
 
 export default function Created() {
+  const [load, setLoad] = useState(1);
+  const [items, setItems] = useState<RecruitCardData[]>([]);
+
+  useEffect(() => {
+    const start = LOAD_SIZE * (load - 1);
+    const end = LOAD_SIZE * load;
+    const newItems = mockCard.slice(start, end);
+    setItems((prev) => [...prev, ...newItems]);
+  }, [load]);
+
+  const handleInView = () => {
+    if (items.length < mockCard.length) {
+      setLoad((prev) => prev + 1);
+    }
+  };
+
+  const renderCard = (item: RecruitCardData) => (
+    <Link key={item.id} href={`de/${item.id}`}>
+      <Card.Thumbnail
+        thumbnail={item.thumbnail}
+        liked={item.liked}
+        alt={item.name}
+        isLike={false}
+      />
+      <Card.TagList tags={item.genres} />
+      <Card.TitleBlock title={item.name} author={item.author} />
+      <Card.Footer
+        status={CARD_STATE.COMPLETED}
+        totalCurrent={item.totalCurrent}
+        totalRecruit={item.totalRecruit}
+        recruitDeadline={item.recruitDeadline}
+        member={item.member}
+      />
+    </Link>
+  );
+
   return (
-    <div className="pc:grid-cols-4 grid grid-cols-1 gap-x-5 gap-y-10 px-[9rem] py-[1.25rem]">
-      {mockCard.map((item: RecruitCardData) => (
-        <Link key={item.id} href={`de/${item.id}`}>
-          <Card.Thumbnail
-            thumbnail={item.thumbnail}
-            liked={item.liked}
-            alt={item.name}
-            isLike={false}
-          />
-          <Card.TagList tags={item.genres} />
-          <Card.TitleBlock title={item.name} author={item.author} />
-          <Card.Footer
-            status={CARD_STATE.COMPLETED}
-            totalCurrent={item.totalCurrent}
-            totalRecruit={item.totalRecruit}
-            recruitDeadline={item.recruitDeadline}
-            member={item.member}
-          />
-        </Link>
-      ))}
-    </div>
+    <InfinityScroll<RecruitCardData>
+      list={items}
+      item={renderCard}
+      emptyText="참여 중인 모집이 없습니다."
+      onInView={handleInView}
+      hasMore={items.length < mockCard.length}
+    />
   );
 }
