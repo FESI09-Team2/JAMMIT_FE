c1d6d762e411dde5c5fcaada296f5a036fbadd14
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-06-03 13:18:07 +0900

    feat: gnb 디자인 적용

--- src/components/commons/Gnb.tsx
+++ src/components/commons/Gnb.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import DefaultProfileImage from '@/assets/icons/ic_default_profile.svg';
+import JammitLogo from '@/assets/icons/ic_jammit_logo.svg';
 
 export default function Gnb() {
   const pathname = usePathname();
@@ -21,22 +22,18 @@ export default function Gnb() {
 
   return (
     <header className="fixed top-0 left-0 z-50 w-full">
-      <div className="flex h-[3.75rem] w-full items-center justify-center bg-black px-[1.75rem]">
-        <div className="flex w-full max-w-[74.875rem] items-center justify-between text-white">
+      <div className="flex h-[3.75rem] w-full items-center justify-center bg-[#151515] px-[1.75rem]">
+        <div className="flex w-full max-w-[84rem] items-center justify-between text-white">
           <nav className="flex gap-[1.5rem]">
-            <Link
-              href="/"
-              data-active={pathname === '/'}
-              className="opacity-80 data-[active=true]:opacity-100"
-            >
-              JAMMIT
+            <Link href="/" data-active={pathname === '/'}>
+              <JammitLogo />
             </Link>
             {navItems.map(({ href, label }) => (
               <Link
                 key={href}
                 href={href}
                 data-active={pathname === href}
-                className="opacity-80 data-[active=true]:opacity-100"
+                className="font-semibold text-gray-300 data-[active=true]:font-bold data-[active=true]:text-gray-100"
               >
                 {label}
               </Link>
@@ -71,7 +68,7 @@ export default function Gnb() {
             ) : (
               <Link
                 data-active={pathname === '/login'}
-                className="opacity-80 data-[active=true]:opacity-100"
+                className="font-semibold opacity-80 data-[active=true]:font-bold data-[active=true]:opacity-100"
                 href="/login"
               >
                 로그인
