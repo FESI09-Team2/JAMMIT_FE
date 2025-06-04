906798f581159fb39ee435c8ad5a2f28830b5f6f
whdjh <whdjh98@naver.com>
2025-06-04 18:14:56 +0900

    feat: get함수로 사용자 데이터 불러옴

--- src/components/products/mypage/UserCard.tsx
+++ src/components/products/mypage/UserCard.tsx
@@ -5,12 +5,13 @@ import DefaultProfileImage from '@/assets/icons/ic_default_profile.svg';
 import EditIcon from '@/assets/icons/ic_edit.svg';
 import ModalEdit from '@/components/commons/Modal/ModalEdit';
 import { useUpdateProfile } from '@/hooks/queries/user/useUpdateProfile';
+import { useUserMeQuery } from '@/hooks/queries/user/useUserMeQuery';
 import { EditFormData } from '@/types/modal';
-import { BandSession, Genre } from '@/types/tags';
 
 export default function UserCard() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const updateProfile = useUpdateProfile();
+  const { data: user } = useUserMeQuery();
 
   const handleProfileEdit = () => {
     setIsModalOpen(true);
@@ -36,9 +37,8 @@ export default function UserCard() {
         </div>
         <div className="flex flex-col text-gray-100">
           <div className="flex items-center gap-[0.625rem]">
-            {/* TODO: GET API 가져오기*/}
             <p className="text-[1.5rem] leading-[2.4rem] font-bold">
-              사용자닉네임
+              {user.username}
             </p>
             <button type="submit" onClick={handleProfileEdit}>
               <EditIcon width={18} height={18} />
@@ -46,23 +46,21 @@ export default function UserCard() {
           </div>
           <div className="flex items-center gap-[1rem] text-sm font-medium">
             <p>담당 세션</p>
-            {/* TODO: GET API 가져오기*/}
             <div className="h-[2rem] w-auto rounded-lg bg-[#34343A] px-[0.75rem] py-[0.375rem]">
-              일렉 기타
+              {user.preferredBandSessions}
             </div>
             <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>선호 장르</p>
-            {/* TODO: GET API 가져오기*/}
             <div className="h-[2rem] w-auto rounded-lg bg-[#34343A] px-[0.75rem] py-[0.375rem]">
-              팝
+              {user.preferredGenres}
             </div>
             <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>개설모임수</p>
-            {/* TODO: GET API 가져오기*/}
+            {/* TODO: API 업데이트 후 GET API 가져오기*/}
             <p>8</p>
             <div className="h-[1.25rem] w-[0.0938rem] bg-gray-500" />
             <p>작성글수</p>
-            {/* TODO: GET API 가져오기*/}
+            {/* TODO: API 업데이트 후 GET API 가져오기*/}
             <p>20</p>
           </div>
         </div>
@@ -74,12 +72,12 @@ export default function UserCard() {
           onSubmit={handleModalSubmit}
           // TODO: GET API 가져오기
           initialData={{
-            email: 'test@naver.com',
-            password: '1234',
-            username: 'test',
-            image: undefined,
-            preferredBandSessions: [BandSession.ELECTRIC_GUITAR],
-            preferredGenres: [Genre.ROCK],
+            email: user.email,
+            password: 'undefined',
+            username: user.username,
+            image: user.profileImagePath,
+            preferredBandSessions: user.preferredBandSessions,
+            preferredGenres: user.preferredGenres,
           }}
         />
       )}
