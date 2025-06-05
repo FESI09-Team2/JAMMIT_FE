26ac19fae9ff4f9679ccef5b5362514420848cd9
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-06-05 11:35:07 +0900

    refactor: 회원가입 및 로그아웃 클린 아키텍쳐 기반 분리

--- src/utils/authService.ts
+++ src/utils/authService.ts
@@ -1,9 +1,20 @@
 import { tokenService } from '@/utils/tokenService';
-import { LoginRequest } from '@/types/auth';
+import { LoginRequest, SignupRequest } from '@/types/auth';
 import { postLogin } from '@/lib/auth/login';
+import { postSignup } from '@/lib/auth/signup';
+import { queryClient } from '@/lib/react-query';
 
 export const login = async (loginRequest: LoginRequest): Promise<void> => {
   const { accessToken, refreshToken } = await postLogin(loginRequest);
   tokenService.setAccessToken(accessToken);
   tokenService.setRefreshToken(refreshToken);
+};
+
+export const signup = async (signupRequest: SignupRequest): Promise<void> => {
+  await postSignup(signupRequest);
+};
+
+export const logout = async (): Promise<void> => {
+  tokenService.clearAllTokens();
+  queryClient.invalidateQueries({ queryKey: ['me'] });
 };
