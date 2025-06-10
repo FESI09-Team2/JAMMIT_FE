12c1fc14ab2c9b93337315d45fc55d93c1c63683
whdjh <whdjh98@naver.com>
2025-06-10 10:13:48 +0900

    feat: API & interface추가

--- src/types/gather.ts
+++ src/types/gather.ts
@@ -1,6 +1,7 @@
 // import { RecruitCardData } from '@/types/card';
 // MEMO: 임시 방편, recurit 변경되면 맞출게요
 import { GatheringCard } from '@/types/card';
+import { GenreType, BandSessionType, GatheringStatus } from './tags';
 
 /** gather에 대한 get을 했을때 오는 response */
 export interface GatheringsResponse {
@@ -21,4 +22,33 @@ export interface GetUserGatheringsParams {
   size: number;
   /** 취소된 모임 포함 여부 */
   includeCanceled?: boolean;
+}
+
+/** gather를 생성할 때 보내야할 request */
+export interface RegisterGatheringsRequest {
+  name: string;
+  thumbnail: string;
+  place: string;
+  description: string;
+  gatheringDateTime: string;
+  recruitDateTime: string;
+  genres: GenreType[];
+  status: GatheringStatus;
+  totalRecruitCount: number;
+  gatheringsessions: {
+    bandSession: BandSessionType;
+    recruitCount: number;
+    currentCount: number;
+  }[];
+}
+
+/** gather를 생성할 때 받아야할 response */
+export interface RegisterGatheringsResponse {
+  id: number;
+  name: string;
+  message: string;
+  gatheringDateTime: string;
+  recruitDeadline: string;
+  thumbnail: string;
+  status: GatheringStatus;
 }
