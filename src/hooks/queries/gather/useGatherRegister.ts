31d4c876aa42ce33bbd6284dfe40202c544e1b40
whdjh <whdjh98@naver.com>
2025-06-10 16:04:55 +0900

    feat: 상세페이지로 이동

--- src/hooks/queries/gather/useGatherRegister.ts
+++ src/hooks/queries/gather/useGatherRegister.ts
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { postRegisterGatherings } from '@/lib/gathering/register';
 import { handleAuthApiError } from '@/utils/authApiError';
@@ -6,13 +7,21 @@ import {
   RegisterGatheringsResponse,
 } from '@/types/gather';
 
-export const useGatherRegister = () =>
-  useMutation<RegisterGatheringsResponse, Error, RegisterGatheringsRequest>({
+export const useGatherRegister = () => {
+  const router = useRouter();
+
+  return useMutation<
+    RegisterGatheringsResponse,
+    Error,
+    RegisterGatheringsRequest
+  >({
     mutationFn: postRegisterGatherings,
     onSuccess: (data) => {
       alert(`모임이 성공적으로 생성되었습니다. ID: ${data.id}`);
+      router.push(`/group/${data.id}`);
     },
     onError: (error) => {
       handleAuthApiError(error, '모임생성에 실패했습니다.');
     },
   });
+};
