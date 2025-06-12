964981ee0ec5143fd99f6428ae39af68c0d59356
강은우 <80903685+eunwoo66@users.noreply.github.com>
2025-06-12 13:14:25 +0900

    feat: 로그인, 회원가입 모달 및 토스트 적용

--- src/utils/authApiError.ts
+++
@@ -1,8 +0,0 @@
-export const handleAuthApiError = (error: unknown, fallbackMessage: string) => {
-  if (error instanceof Error) {
-    // TODO: 모달, 토스트 등 적용 필요
-    alert(error.message);
-  } else {
-    alert(fallbackMessage);
-  }
-};
