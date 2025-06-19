8bb88e0082761ec5670f444bb5b7a57630b941ab
whdjh <whdjh98@naver.com>
2025-06-19 18:29:03 +0900

    fix: toast 제거

--- src/hooks/queries/auth/useVerifyCodeMutation.ts
+++ src/hooks/queries/auth/useVerifyCodeMutation.ts
@@ -1,6 +1,5 @@
 import { postVerifyCode } from '@/lib/auth/verifycode';
 import { useToastStore } from '@/stores/useToastStore';
-import { handleAuthApiError } from '@/utils/authApiError';
 import { useMutation } from '@tanstack/react-query';
 
 export const useVerifyCodeMutation = () => {
@@ -9,16 +8,12 @@ export const useVerifyCodeMutation = () => {
       postVerifyCode({ email, code }),
     onSuccess: (data) => {
       if (data && data.success === false) {
-        useToastStore.getState();
+        useToastStore
+          .getState()
+          .show(data.message || '인증 코드 확인에 실패했습니다.');
         return;
       }
       useToastStore.getState().show('인증 코드가 확인되었습니다.');
-    },
-    onError: (error) => {
-      handleAuthApiError(error, '인증 코드 전송 중 오류가 발생했습니다.', {
-        section: 'auth',
-        action: 'verify_email_code',
-      });
     },
   });
 };
